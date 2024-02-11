0e25f7e11c50e3f68f6b9b9bdfa24bd111b3a822
Shihab Uddin <105420005+shihab4513@users.noreply.github.com>
2024-02-11 15:36:48 +0600

    test 3 create graph completed

--- test3.py
+++ test3.py
@@ -1,5 +1,5 @@
 import csv
-
+import queue
 
 def csv_file_reader(fileNAme):
     with open(fileNAme, 'r') as file:
@@ -13,9 +13,21 @@ def createGraph(distances):
     for row in distances:
         (source,dest,distance)=row
         if source not in graph:
-            graph[source]={
-                dest:int(distance)
-            }
+            graph[source]={}
+        graph[source][dest]=int(distance)
+    return graph
+
+def dijakstra(start,end,graph):
+    pq=queue.PriorityQueue()
+
+    distances={node:float('infinity') for node in graph}
+    pq.put((0, start))
+    distances[start]=0
+    path={}
+    while pq.empty():
+        (currnt_distance,current_vertice)=pq.get()
+        if(distances[current_vertice])
+
 
 if __name__ == "__main__":
     distances = csv_file_reader('distances.csv')
